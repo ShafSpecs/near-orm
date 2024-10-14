334e6c390bf34c194b7418af4f6f60647c949bb9
ShafSpecs <abbadshaf05@gmail.com>
2024-10-14 12:08:13 +0100

    🤏 minification script

---
+++ minify.js
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import { minify } from "terser";
+
+async function minifyFiles(filePaths) {
+  for (const filePath of filePaths) {
+    const mini = await minify(readFileSync(filePath, 'utf8'), {
+      compress: {
+        keep_infinity: true,
+        keep_fnames: true,
+        keep_classnames: true,
+        passes: 2,
+      },
+    });
+    const minicode = mini.code;
+    writeFileSync(filePath, minicode);
+  }
+}
+
+minifyFiles(["./orm.js"]);
